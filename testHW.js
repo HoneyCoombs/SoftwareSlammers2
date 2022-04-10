d2259c75263fc30f40bc7232e28c140d0b23188e
Dominic Pisano <47585964+DominicPisano@users.noreply.github.com>
2022-04-09 23:26:53 -0400

    Update testHW.js

--- testHW.js
+++ testHW.js
@@ -11,8 +11,6 @@ describe('ComputeHW Test Suite', function () {
     });
   });
 
-
-
   describe('computeHW 2', function () {
     it('Input of 100,90,80 should return 18', function () {
       assert.equal(computeHW.computeHW(100,90,80), 18);
@@ -43,9 +41,40 @@ describe('ComputeHW Test Suite', function () {
     });
   });
 
-  //@Dominic put your test cases here
+  describe('computeHW 7', function () {
+    it('Input of 50.5,50.5,50.5 should return 10.1', function () {
+      assert.equal(computeHW.computeHW(50.5,50.5,50.5), 10.1);
+    });
+  });
 
+  describe('computeHW 8', function () {
+    it('Input of 52,74,73 should return 13.266666666666666', function () {
+      assert.equal(computeHW.computeHW(52,74,73), 13.266666666666666);
+    });
+  });
 
+  describe('computeHW 9', function () {
+    it('Input of 100,97,98 should return 19.666666666666668', function () {
+      assert.equal(computeHW.computeHW(100,97,98), 19.666666666666668);
+    });
+  });
 
+  describe('computeHW 10', function () {
+    it('Input of 100,67,80 should return 16.466666666666665', function () {
+      assert.equal(computeHW.computeHW(100,67,80), 16.466666666666665);
+    });
+  });
+
+  describe('computeHW 11', function () {
+    it('Input of 100,40,47 should return 12.466666666666669', function () {
+      assert.equal(computeHW.computeHW(100,40,47), 12.466666666666669);
+    });
+  });
+
+  describe('computeHW 12', function () {
+    it('Input of 90,90,82 should return 17.466666666666665', function () {
+      assert.equal(computeHW.computeHW(90,90,82), 17.466666666666665);
+    });
+  });
 
 }); //End of ComputeHW Test Suite
